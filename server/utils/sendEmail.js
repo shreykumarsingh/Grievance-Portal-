741129cc7de9a410ba5c0f3e0c1f890a4694aaa3
shreykumarsingh <workmailshreykumarsingh@gmail.com>
2026-05-01 03:06:58 +0530

    Add server folder to repository

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async ({ to, subject, html, text }) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: '"Grievance Portal" <grievanceportalbmsce@gmail.com>',
+      to,
+      subject,
+      html,
+      text,
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    return info;
+  } catch (err) {
+    console.error('Failed to send email:', err);
+    throw err;
+  }
+};
+
+module.exports = sendEmail;
